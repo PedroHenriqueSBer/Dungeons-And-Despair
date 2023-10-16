2b0694729ecb1e89ef240cf945ad77a8b0cd6a0d
PedroHSBernardes <133461209+PedroHenriqueSBer@users.noreply.github.com>
2023-10-16 16:00:52 -0300

    ds

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,7 @@
 import ReactDOM from 'react-dom/client';
-import { GlobalStyle } from './style/GlobalStyle';
-import { Router } from './routes/routes';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <>
-        <Router />
-        <GlobalStyle/>
+        Teste
     </>
 );
--- frontend/src/pages/Inventory/styled.ts
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const Container = styled.div`
-    width: 100%;
-    padding-top: 2rem;
-    display: flex;
-    justify-content: center;
-`
