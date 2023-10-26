f37765e12dd96d53290ef7acc7b5d6dae0b150d2
pedro.bernardes <pedro.bernardes@legalcontrol.com.br>
2023-10-25 23:24:06 -0300

    .

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,29 +1,28 @@
 import { useNavigate } from "react-router";
-import { useUser } from "../../hooks/useUser";
 import icon from './../../assets/icon.png';
 import { Button } from './../../layout/Button/index';
 import { Container } from "./styles";
 
 export const Header = () => {
     const navigate = useNavigate()
-    const {user} = useUser()
-    const Btns = user.id == undefined? 
+    const id = '313132'
+    const Btns = id == undefined? 
     [
         {
-            path: '/signin',
+            path: '/',
             name: 'Signin'
         }
     ] : [
         {
-            path: '/inventory',
+            path: '/',
             name: 'Inventory'
         },
         {
-            path: '/pdfs',
+            path: '/',
             name: 'Pdfs'
         },
         {
-            path: '/account',
+            path: '/',
             name: 'Account'
         }
     ]
